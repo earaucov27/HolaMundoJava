49a4631b1cf5ff89adfd26113399059ca68a3e4b
earaucov27 <e.araucovalenzuela@gmail.com>
2021-08-10 18:38:39 -0400

    Se agregan lineas de como crear un main

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,4 +2,8 @@ public class HolaMundo {
     public static void main (String[] args){
         System.out.println("Hola Mundo desde Java");
     }
+    
+    //comandos para crear un main automaticamente:
+    //main + tab
+    //psvm + tab
 }
